5a846f6a77f80b3d1136df80ed9f2f8066eb2e8f
Remil <remiliance@yahoo.fr>
2020-05-15 16:26:56 +0200

    first

---
+++ src/main/java/com/onlinetutorialspoint/SpringBootKafkaJsonMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.onlinetutorialspoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaJsonMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaJsonMessagesApplication.class, args);
+	}
+
+}
